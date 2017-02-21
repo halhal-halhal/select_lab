4175641f6599d4d9cec61aac9540d32dafb6c67d
halspring <hal.webworks@gmail.com>
2017-02-21 20:59:12 +0900

    StudentとUserの連動以外

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   resources :labs
   resources :students
   root 'labs#index'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

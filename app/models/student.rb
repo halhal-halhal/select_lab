88acbc524673e30acea0504d8ee8008e9b3d961d
halspring <hal.webworks@gmail.com>
2017-02-26 20:43:26 +0900

    協力者リスト表示

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,8 @@ class Student < ApplicationRecord
   validates :name, presence: true
   validates :first, presence: true
   validates :rank, presence: true
-  validates :code, presence: true
+
+
+  VALID_CODE =  /1[34]+TI0[0-9][0-9]/
+  validates :code, presence: true, format: {with: VALID_CODE}
 end
